ee4daa6ba77f66c07bcd701ed0a7b3afcf77fef8
David Olivier Jamin <david.jamin@cern.ch>
2018-04-24 13:53:06 +0200

    update to Michele's version

--- fit.py
+++ fit.py
@@ -1,71 +1,98 @@
 import ROOT as r
-r.gROOT.SetBatch(r.kTRUE)
-infile = r.TFile.Open('/afs/cern.ch/user/d/djamin/public/FCC/20180403_tth_boosted/tmp/root_ttH_full_2gevbins/root_ttH/histos.root')
-Rebin=2
+#r.gROOT.SetBatch(r.kTRUE)
+#infile = r.TFile.Open('/afs/cern.ch/user/d/djamin/public/FCC/20180403_tth_boosted/tmp/root_ttH_full_2gevbins/root_ttH/histos.root')
+infile = r.TFile.Open('/afs/cern.ch/work/s/selvaggi/private/Analysis/FCC/FlatTreeAnalyzer/tth_boosted/root_ttH/histos.root')
+Rebin=1
 
 
+def makePseudoExp(tth_datahist, ttz_datahist, sig, alpha, ntth, nttz, alpha_val, alpha_err):
+ # generate datasets
+
+    print '================================================================================================='
+    print '================================================================================================='
+
+
+    gen_tth = p_tth.generate(r.RooArgSet(x), ntth, r.RooFit.Extended())
+    gen_ttz = p_ttz.generate(r.RooArgSet(x), nttz, r.RooFit.Extended())
+
+    d_ttcomb = gen_tth
+    d_ttcomb.append(gen_ttz)
+
+    #d_ttcomb.plotOn(frame)
+
+    sig.fitTo(d_ttcomb, r.RooFit.SumW2Error(False), r.RooFit.PrintLevel(-1))
+    #sig.plotOn(frame, r.RooFit.LineColor(r.kBlack))
+    #frame.Draw()
+    #alpha.Print()
+
+    print alpha.getError(), alpha.getVal()
+    alpha.Print()
+
+    #ratio = alpha.getVal()*(ntth+nttz)/ntth
+    #ratio_err = alpha.getError()*(ntth+nttz)/ntth
+    ratio = (1-alpha.getVal())/(alpha.getVal())
+    ratio_err = alpha.getError()
+
+
+    print ratio, ratio_err
+
+    alpha_err.Fill(ratio_err)
+    alpha_val.Fill(ratio)
+
+
+
+x = r.RooRealVar("x", "mjj", 0, 300)
 #######ttH
-h_tth=infile.Get('ttH_sel1_h_mjj_l2')
+h_tth=infile.Get('ttH_sel3_h_mbb')
 h_tth.Rebin(Rebin)
-x_tth = r.RooRealVar("x_tth", "mjj", 50, 140)
-d_tth = r.RooDataHist("d_tth", "d_tth", r.RooArgList(x_tth),r.RooFit.Import(h_tth))
-frame = x_tth.frame(r.RooFit.Title("CB fit ttH"))
-d_tth.plotOn(frame, r.RooFit.MarkerColor(r.kRed), r.RooFit.LineColor(r.kRed) )
+d_tth = r.RooDataHist("d_tth", "d_tth", r.RooArgList(x),r.RooFit.Import(h_tth))
+p_tth = r.RooHistPdf("p_tth", "p_tth", r.RooArgSet(x),d_tth)
+frame = x.frame(r.RooFit.Title("ttH"))
+p_tth.plotOn(frame, r.RooFit.MarkerColor(r.kRed), r.RooFit.LineColor(r.kRed) )
 
-# Fit a CB p.d.f to the data
-m0_tth    = r.RooRealVar("m0_tth","m0_tth",125,0,300)
-alpha_tth = r.RooRealVar("alpha_tth","alpha_tth",1,0,10)
-n_tth     = r.RooRealVar("n_tth","n_tth",1, 0.1,10)
-sigma_tth = r.RooRealVar("sigma_tth", "sigma_tth", 20, 1, 100)
-
-CB_tth = r.RooCBShape("CB_tth", "CB_tth", x_tth, m0_tth, sigma_tth, alpha_tth, n_tth)
-CB_tth.fitTo(d_tth)
-CB_tth.plotOn(frame, r.RooFit.LineColor(r.kRed))
 
 #######ttZ
-h_ttz=infile.Get('ttZ_sel1_h_mjj_l2')
+h_ttz=infile.Get('ttZ_sel3_h_mbb')
 h_ttz.Rebin(Rebin)
-x_ttz = r.RooRealVar("x_ttz", "mjj", 50, 100)
-d_ttz = r.RooDataHist("d_ttz", "dttz", r.RooArgList(x_ttz),r.RooFit.Import(h_ttz))
-frame2 = x_ttz.frame(r.RooFit.Title("CB fit ttZ"))
-d_ttz.plotOn(frame2)
+d_ttz = r.RooDataHist("d_ttz", "dttz", r.RooArgList(x),r.RooFit.Import(h_ttz))
+p_ttz = r.RooHistPdf("p_ttz", "p_ttz", r.RooArgSet(x),d_ttz)
+#frame2 = x.frame(r.RooFit.Title("CB fit ttZ"))
+p_ttz.plotOn(frame)
+#frame.Draw()
 
-# Fit a CB p.d.f to the data
-m0_ttz    = r.RooRealVar("m0_ttz","m0_ttz",90,0,300)
-alpha_ttz = r.RooRealVar("alpha_ttz","alpha_ttz",1,0,10)
-n_ttz     = r.RooRealVar("n_ttz","n_ttz",1, 0.1,10)
-sigma_ttz = r.RooRealVar("sigma_ttz", "sigma_ttz", 20, 1, 100)
+# add pdfs
 
-CB_ttz = r.RooCBShape("CB_ttz", "CB_ttz", x_ttz, m0_ttz, sigma_ttz, alpha_ttz, n_ttz)
-CB_ttz.fitTo(d_ttz)
-CB_ttz.plotOn(frame2, r.RooFit.LineColor(r.kBlack))
+alpha = r.RooRealVar("alpha", "fraction of component in signal", 0.7167035, 0., 1.)
+#alpha.setConstant(True)
+sig = r.RooAddPdf("sig", "Signal", r.RooArgList(p_tth, p_ttz),r.RooArgList(alpha))
+#sig.plotOn(frame, r.RooFit.MarkerColor(r.kBlack), r.RooFit.LineColor(r.kBlack))
+frame.Draw()
+#
+#def :
 
+ntth = 158836
+nttz = 85785
 
-#######COMB
-h_ttcomb=h_ttz+h_tth
-x_ttcomb = r.RooRealVar("x_ttcomb", "mjj", 50, 140)
-d_ttcomb = r.RooDataHist("d_ttcomb", "d_ttcomb", r.RooArgList(x_ttcomb),r.RooFit.Import(h_ttcomb))
-frame3 = x_ttcomb.frame(r.RooFit.Title("CB fit ttcomb"))
-d_ttcomb.plotOn(frame3)
-CB_ttz = r.RooCBShape("CB_ttz", "CB_ttz", x_ttcomb, m0_ttz, sigma_ttz, alpha_ttz, n_ttz)
-CB_tth = r.RooCBShape("CB_tth", "CB_tth", x_ttcomb, m0_tth, sigma_tth, alpha_tth, n_tth)
+ntth = int(0.99*ntth)
+nttz = int(0.99*nttz)
 
-sigfrac = r.RooRealVar("sigfrac", "fraction of component in signal", 0.8, 0., 1.)
-sig = r.RooAddPdf("sig", "Signal", r.RooArgList(CB_tth, CB_ttz),r.RooArgList(sigfrac))
-sig.fitTo(d_ttcomb)
-sig.plotOn(frame3, r.RooFit.LineColor(r.kBlack))
+alpha_val = r.TH1F("alpha_val", "alpha_val", 100, 0.50, 0.58)
+alpha_err = r.TH1F("alpha_err", "alpha_err", 100, 0.001, 0.003)
 
 
-frame4 = x_ttcomb.frame(r.RooFit.Title("CB fit ttcomb"))
-d_ttcomb.plotOn(frame4)
-sig.plotOn(frame4, r.RooFit.LineColor(r.kBlack))
-
-sigtth = r.RooArgSet(CB_tth)
-sig.plotOn(frame4, r.RooFit.Components(sigtth), r.RooFit.LineStyle(r.kDashed))
-sigttz = r.RooArgSet(CB_ttz)
-sig.plotOn(frame4, r.RooFit.Components(sigttz), r.RooFit.LineStyle(r.kDashed))
+for i in xrange(1000):
+    makePseudoExp(p_tth, p_ttz, sig, alpha, ntth, nttz, alpha_val, alpha_err)
 
 
+outfile = r.TFile('tth_fit.root','RECREATE')
+alpha_val.Write()
+alpha_err.Write()
+
+
+
+#input("Press Enter to continue...")
+
+'''
 
 # Draw all frames on a canvas
 c = r.TCanvas("CBfit", "CBfit", 800, 800)
@@ -94,4 +121,4 @@ c1 = r.TCanvas("CBfit_comb", "CBfit", 600, 800)
 frame3.Draw()
 c1.SaveAs("CBfit_comb.png")
 
-
+'''
